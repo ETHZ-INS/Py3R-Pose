26b03f0980e421b2c849402a83ffb6787843fdf2
Marcel Schmutz <marcel.schmutz@hest.ethz.ch>
2025-11-10 11:06:31 +0100

    Added xy property to PosePoint

--- src/py3r/pose/core/types/point.py
+++ src/py3r/pose/core/types/point.py
@@ -8,5 +8,9 @@ class PosePoint:
     # conf is None for human-annotated points
     conf: float | None = None
 
+    @property
+    def xy(self) -> tuple[float, float]:
+        return self.x, self.y
+
     def as_dict(self) -> dict:
         return {"x": self.x, "y": self.y, "conf": self.conf}
