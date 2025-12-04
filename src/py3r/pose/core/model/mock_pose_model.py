15c7b02ba7dad634136e88713cc19a0c704edc1c
Marcel Schmutz <marcel.schmutz@hest.ethz.ch>
2025-12-04 14:42:20 +0100

    Require pose models to implement both _predict and _predict_batch

--- src/py3r/pose/core/model/mock_pose_model.py
+++ src/py3r/pose/core/model/mock_pose_model.py
@@ -1,4 +1,6 @@
-from typing import List, Any
+from typing import List, Any, Union, Sequence
+
+import numpy as np
 
 from py3r.pose.core.model.pose_model import PoseModel
 from py3r.pose.core.types import PoseInstance, PoseInstanceType
@@ -15,5 +17,8 @@ class MockPoseModel(PoseModel):
                 unique_instance_types[instance.type.name] = instance.type
         return list(unique_instance_types.values())
 
-    def _predict(self, _img: Any) -> List[PoseInstance]:
+    def _predict(self, _img: Union[np.ndarray, Any]) -> List[PoseInstance]:
         return self.instances
+
+    def _predict_batch(self, batch: Union[Sequence[np.ndarray], Any]) -> List[List[PoseInstance]]:
+        return [self.instances for _ in batch]
