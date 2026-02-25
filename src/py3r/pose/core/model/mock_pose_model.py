9943f1ab258b655111284f4f4e3c7daca2b6e889
Marcel Schmutz <marcel.schmutz@hest.ethz.ch>
2026-02-25 13:21:27 +0100

    Limiting decimals in final csv to 2

--- src/py3r/pose/core/model/mock_pose_model.py
+++ src/py3r/pose/core/model/mock_pose_model.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from typing import List, Any, Union, Sequence
 
 import numpy as np
@@ -7,8 +8,9 @@ from py3r.pose.core.types import PoseInstance, PoseInstanceType
 
 
 class MockPoseModel(PoseModel):
-    def __init__(self, instances: List[PoseInstance]):
+    def __init__(self, instances: List[PoseInstance], noise: float = 0.01):
         self.instances = instances
+        self.noise = noise
 
     def get_instance_types(self) -> List[PoseInstanceType]:
         unique_instance_types = {}
@@ -18,7 +20,13 @@ class MockPoseModel(PoseModel):
         return list(unique_instance_types.values())
 
     def _predict(self, _img: Union[np.ndarray, Any]) -> List[PoseInstance]:
-        return self.instances
+        # Add random noise to each point
+        instances = [deepcopy(instance) for instance in self.instances]
+        for instance in instances:
+            for point in instance.points:
+                point.x += np.random.normal(0, self.noise)
+                point.y += np.random.normal(0, self.noise)
+        return instances
 
     def _predict_batch(self, batch: Union[Sequence[np.ndarray], Any]) -> List[List[PoseInstance]]:
-        return [self.instances for _ in batch]
+        return [self._predict(img) for img in batch]
